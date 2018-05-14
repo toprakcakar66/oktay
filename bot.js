3cbce6facfda66a6ca9138e870eee698051772dc
toprakcakar66 <39004667+toprakcakar66@users.noreply.github.com>
2018-05-14 21:54:31 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,16 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+var prefix = "!";
+
+client.on('ready', () => {
+  console.log(`Bot:Başarıyla giriş yaptı!`);
+});
+
+client.on('message', msg => {
+  if (msg.content.toLowerCase() === 'sa') {
+    msg.reply("Aleyküm Selam");
+  }
+});
+
+client.login("MzA5MDM0MjYwMTAxMjAxOTIx.DW3a2w.rh4xxbbtki6xaQ9i3gl4VLl40fY");
